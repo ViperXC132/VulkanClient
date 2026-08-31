2c61377f64dd8ecbc7d446ce89744324f7efe9d8
Viper Davis <viperxc2204@gmail.com>
2026-08-31 21:23:27 +0100

    Fix main client bootstrap for Yarn 1.21.11

--- src/main/java/com/viper/vulkanclient/VulkanClientClient.java
+++ src/main/java/com/viper/vulkanclient/VulkanClientClient.java
@@ -1,6 +1,5 @@
 package com.viper.vulkanclient;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import com.viper.vulkanclient.core.FeatureRegistry;
 import com.viper.vulkanclient.core.Module;
 import com.viper.vulkanclient.core.ModuleManager;
@@ -8,30 +7,29 @@ import com.viper.vulkanclient.module.hud.ClientHudRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public final class VulkanClientClient implements ClientModInitializer {
-    private static final KeyMapping.Category KEY_CATEGORY = KeyMapping.Category.create("key.category.vulkanclient.client");
-    private static KeyMapping openMenu;
+    private static final KeyBinding openMenu = new KeyBinding(
+            "key.vulkanclient.open_menu",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "key.category.vulkanclient.client"
+    );
     private static ModuleManager modules;
 
     @Override
     public void onInitializeClient() {
         modules = FeatureRegistry.create();
-        openMenu = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.vulkanclient.open_menu",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_RIGHT_SHIFT,
-                KEY_CATEGORY
-        ));
-
+        KeyBindingHelper.registerKeyBinding(openMenu);
         ClientHudRenderer.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (openMenu.consumeClick()) {
-                if (client.screen == null) client.setScreen(new VulkanClientScreen());
+            while (openMenu.wasPressed()) {
+                if (client.currentScreen == null) client.setScreen(new VulkanClientScreen());
             }
             for (Module module : modules.all()) {
                 if (module.enabled()) module.onTick();
@@ -39,8 +37,8 @@ public final class VulkanClientClient implements ClientModInitializer {
         });
     }
 
-    public static Minecraft minecraft() {
-        return Minecraft.getInstance();
+    public static MinecraftClient minecraft() {
+        return MinecraftClient.getInstance();
     }
 
     public static ModuleManager modules() {

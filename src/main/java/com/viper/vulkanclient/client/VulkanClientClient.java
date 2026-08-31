98f8231836504b9b5ad9cca7a9142a88d2b05f71
Viper Davis <viperxc2204@gmail.com>
2026-08-31 21:10:58 +0100

    Wire persistent client profiles

--- src/main/java/com/viper/vulkanclient/client/VulkanClientClient.java
+++ src/main/java/com/viper/vulkanclient/client/VulkanClientClient.java
@@ -7,11 +7,14 @@ import com.viper.vulkanclient.client.module.visual.ZoomModule;
 import com.viper.vulkanclient.client.module.visual.LowFireModule;
 import com.viper.vulkanclient.client.module.visual.LowShieldModule;
 import com.viper.vulkanclient.client.hud.HudManager;
+import com.viper.vulkanclient.client.profile.ProfileManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public final class VulkanClientClient implements ClientModInitializer {
     public static final ModuleManager MODULES = new ModuleManager();
     public static final HudManager HUD = new HudManager();
+    public static ProfileManager PROFILES;
 
     @Override
     public void onInitializeClient() {
@@ -21,5 +24,6 @@ public final class VulkanClientClient implements ClientModInitializer {
         MODULES.register(new LowFireModule());
         MODULES.register(new LowShieldModule());
         HUD.registerDefaults();
+        PROFILES = new ProfileManager(MODULES, FabricLoader.getInstance().getConfigDir().resolve("vulkanclient/profiles"));
     }
 }

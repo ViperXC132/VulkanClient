6fbb51377e91933bfd44d1b2ec217def7d6e8545
Viper Davis <viperxc2204@gmail.com>
2026-08-31 21:07:52 +0100

    Implement Fullbright

---
+++ src/main/java/com/viper/vulkanclient/client/module/visual/FullbrightModule.java
@@ -0,0 +1,22 @@
+package com.viper.vulkanclient.client.module.visual;
+
+import com.viper.vulkanclient.client.module.Module;
+import net.minecraft.client.MinecraftClient;
+
+public final class FullbrightModule extends Module {
+    private double previousGamma = -1.0;
+
+    public FullbrightModule() { super("fullbright", "Fullbright", Category.VISUAL); }
+
+    @Override public void onEnable() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        previousGamma = client.options.getGamma().getValue();
+        client.options.getGamma().setValue(16.0);
+    }
+
+    @Override public void onDisable() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (previousGamma >= 0.0) client.options.getGamma().setValue(previousGamma);
+        previousGamma = -1.0;
+    }
+}

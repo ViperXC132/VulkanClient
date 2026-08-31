5325821f5b96f1bbc498930ce6d969e10058917d
Viper Davis <viperxc2204@gmail.com>
2026-08-31 21:02:05 +0100

    Fix 1.21.11 key category and wire feature registry

--- src/main/java/com/viper/vulkanclient/VulkanClientClient.java
+++ src/main/java/com/viper/vulkanclient/VulkanClientClient.java
@@ -1,6 +1,9 @@
 package com.viper.vulkanclient;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.viper.vulkanclient.core.FeatureRegistry;
+import com.viper.vulkanclient.core.Module;
+import com.viper.vulkanclient.core.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -9,15 +12,18 @@ import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 
 public final class VulkanClientClient implements ClientModInitializer {
+    private static final KeyMapping.Category KEY_CATEGORY = KeyMapping.Category.create("key.category.vulkanclient.client");
     private static KeyMapping openMenu;
+    private static ModuleManager modules;
 
     @Override
     public void onInitializeClient() {
+        modules = FeatureRegistry.create();
         openMenu = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.vulkanclient.open_menu",
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_RIGHT_SHIFT,
-                "category.vulkanclient"
+                KEY_CATEGORY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
@@ -26,10 +32,18 @@ public final class VulkanClientClient implements ClientModInitializer {
                     client.setScreen(new VulkanClientScreen());
                 }
             }
+            for (Module module : modules.all()) {
+                if (module.enabled()) module.onTick();
+            }
         });
     }
 
     public static Minecraft minecraft() {
         return Minecraft.getInstance();
+    }
+
+    public static ModuleManager modules() {
+        if (modules == null) modules = FeatureRegistry.create();
+        return modules;
     }
 }
